77722fb5e8b3d9377d43ee769deed68269eadc25
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-09-22 20:20:35 +0600

    Created single page app with flask and vue.js as in tutorial

---
+++ client/.eslintrc.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint import/no-extraneous-dependencies: ["error", {options: ...}] */
+rules:
+  import/no-extraneous-dependencies: [error, { devDependencies: true }]
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Ping from '../components/Ping.vue';
 import Books from '../components/Books.vue';
+import Ping from '../components/Ping.vue';
 
 Vue.use(Router);
 
@@ -10,14 +10,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
-    },
-    {
       path: '/',
       name: 'Books',
       component: Books,
+    },
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
     },
   ],
 });
