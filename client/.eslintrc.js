15faa09d37160e2584fc9fe22e15aaecc0011d71
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-09-26 19:43:26 +0600

    client-react-table-library Now page contains table, filled from server via API

--- client/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint import/no-extraneous-dependencies: ["error", {options: ...}] */
-rules:
-  import/no-extraneous-dependencies: [error, { devDependencies: true }]
